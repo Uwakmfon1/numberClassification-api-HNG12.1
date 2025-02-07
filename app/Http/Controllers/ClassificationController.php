bc08ffa7a7870d9d051a888cbd2573c77b68b055
Uwakmfon1 <udohuwakmfon2@gmail.com>
2025-02-07 09:29:52 +0100

    feat: set to production

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Http\Controllers\ClassificationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 
 Route::get('number/{temp}',[ClassificationController::class,'index']);
-
-//
